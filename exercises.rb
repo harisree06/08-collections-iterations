6ef59c4b84fa01b3e0f0b018a2f96ec5318e3334
Lan Phan <lanc_phan@hotmail.com>
2017-09-20 15:54:32 -0400

    setup the array and hashes

---
+++ exercises.rb
@@ -0,0 +1,28 @@
+# Save each of the following arrays and hashes into variables. You will use them throughout the assignment.
+#
+# Eg. fav_colours = ...
+#
+# Create an array for each item below that contains the given information:
+#
+# your favourite colours
+# the age of you and your siblings/cousins/friends
+# flip a coin 5 times and record whether or not the result was 'heads'
+# your three favourite performing artists
+# your favourite colours again (this time as symbols instead of strings)
+
+# Create a hash for each item below that contains the given information:
+# three words and their definitions
+# your favourite movie names and their year of creation
+# three cities of the world and their population
+# the names of your siblings/cousins/friends and their ages
+# After you've done that you should run your code to make sure there aren't any syntax errors. You should get in the habit of running your code after each step in order to catch any mistakes as soon as they're introduced.
+
+fav_colours = ["red","blue","purple","yellow","green"]
+age = ["5","10","15","20","25"]
+coin_flip = ["head","head","head","tail","tail"]
+artists = ["John Legend","Linkin Park","The Weeknd"]
+fav_colours_symbol = [colour_1:"red",colour_2:"blue",colour_3:"purple",colour_4:"yellow",colour_5:"green"]
+
+word_definition = {sad: 'unhappy', happy: 'cheerful',angry: 'mad'}
+cities_population = {toronto: '5', tokyo: '36', new_york: '12'}
+people_ages = {cindy:'5',jessica:'10',michael:'15',john:'20',chris:'25'}
