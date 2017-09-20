fedef7a2ca982736162944ea43a2e06707bf9b0d
Lan Phan <lanc_phan@hotmail.com>
2017-09-20 16:16:46 -0400

    printing out some of the array and hashes

--- exercises.rb
+++ exercises.rb
@@ -24,5 +24,25 @@ artists = ["John Legend","Linkin Park","The Weeknd"]
 fav_colours_symbol = [colour_1:"red",colour_2:"blue",colour_3:"purple",colour_4:"yellow",colour_5:"green"]
 
 word_definition = {sad: 'unhappy', happy: 'cheerful',angry: 'mad'}
+movie_year = {inception:'2010',IT:'2017',jurassic_park:'1993',mean_girls:'2003'}
 cities_population = {toronto: '5', tokyo: '36', new_york: '12'}
 people_ages = {cindy:'5',jessica:'10',michael:'15',john:'20',chris:'25'}
+
+# Print out the array of coin flips.
+puts coin_flip
+
+# Print out the first element of the array of your favourite colours.
+puts "The first element of the colour array is\n"
+puts fav_colours[0].upcase
+
+# Output the sorted version of the array of your friends and family members' ages.
+puts "The family members' ages in order: #{age.sort}"
+
+# Add a new baby (0 years old) to the array of your family's ages.
+age << '0'
+puts age.sort
+
+# Using the hash of movie information, access and print the year of one of the movies.
+puts The movie_year[:inception]
+
+# If you haven't done so recently, now would be a good time to check that your code works and commit once it does.
