0ff23f58322e7d588c9fb0b68b6bb70d3e84cb08
Lan Phan <lanc_phan@hotmail.com>
2017-09-21 15:11:06 -0400

    exercise 10: Bitmaker number of students is done

--- exercise10.rb
+++ exercise10.rb
@@ -14,16 +14,57 @@ puts "\nThese are the following Bitmaker past students' number:"
 bitmaker_students(students)
 
 # Add cohort 4, which had 43 students, to the hash.
+students[:cohort4] = 43
 
 # Use the keys method to output all of the cohort names.
-#
-# The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
-#
+
+def cohort_key(students)
+  students.each_key do |cohort|
+    puts "#{cohort}"
+  end
+end
+
+puts "\nThese are the following cohorts:"
+cohort_key(students)
+
+puts "\nThe classrooms have been expanded! Increase each cohort size by 5% and display the new results."
+
+def increase_size(students)
+  students.each do |cohort,class_size|
+    result = class_size*1.05
+    puts "#{cohort}: #{result}"
+  end
+end
+
+result = []
+increase_size(students)
+
 # Delete the 2nd cohort and redisplay the hash.
-#
+
+students.delete(:cohort2)
+puts "\nThe 2nd cohort is removed and the updated Bitmaker hash is:"
+puts "#{students}"
+
 # BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
-#
+
+def total_number(students)
+  total_number = 0
+  students.each_value do|class_size|
+    total_number += class_size
+  end
+  puts "\nThe total number of students across all cohorts is: #{total_number}"
+end
+
+total_number(students)
+
 # BONUS: Create another similar hash called staff and display it using the display method from above.
-#
+
+staff = {
+  staff1:4,
+  staff2:10,
+  staff3:14
+}
+
+puts staff
 # After you're done, be sure you have committed and pushed everything to your Github repo.
 #
